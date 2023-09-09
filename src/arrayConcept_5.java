1e528712a7eef5e74256f72b40190e583f1e07a7
Dhinesh Kumar Manivannan <93302687+Dhinesh045@users.noreply.github.com>
2023-09-09 21:59:02 +0530

    ArrayConcept

--- src/arrayConcept_5.java
+++ src/arrayConcept_5.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 public class arrayConcept_5 {
     public static void main(String[] args) {
-        int[] index={3,3,4,4,5,5,6,6,7,7,9,0,2,9};
-        int i=index.length;
-        removerDuplicates(index,i);
+        int[] index={3,3,4,4};
+        int[] index2={9,8,7,6};
+        int i1=index.length;
+        int i2=index2.length;
+        int c=i1+i2;
+        int[] c1= new int[c];
+
+        System.arraycopy(index,0,c1,0,i1);
+        System.arraycopy(index2,0,c1,i1,i2);
+        System.out.println(Arrays.toString(c1));
 
     }
-    static int removerDuplicates(){
+
 
 
     }
-}
+
