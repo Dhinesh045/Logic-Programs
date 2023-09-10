d1dd6c8675d96eefb8bf563c677d16b8355b14e0
Dhinesh Kumar Manivannan <93302687+Dhinesh045@users.noreply.github.com>
2023-09-10 13:23:55 +0530

    findCommon

--- src/array/findCommon.java
+++ src/array/findCommon.java
@@ -1,5 +1,26 @@
 package array;
 
 public class findCommon {
-//
+    public static void main(String[] args) {
+        String[] str={"dhinesh","ranjith","murali"};
+        String[] str1={"mani","dhinesh","murali"};
+        commonString(str,str1);
+
+
+    }
+    static void  commonString(String[] str, String[] str1)
+    {
+        for(int i=0;i<str.length;i++)
+            for(int j=0;j<str1.length;j++){
+                if(str[i]==str1[j]){
+                    System.out.println(str[i]);
+
+                }
+
+            }
+
+
+    }
+
+
 }
