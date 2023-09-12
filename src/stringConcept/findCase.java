32a3815d787b0cd5be51c2cc3bbbbb3d26355cd1
Dhinesh Kumar Manivannan <93302687+Dhinesh045@users.noreply.github.com>
2023-09-12 18:57:03 +0530

    case

--- src/stringConcept/findCase.java
+++ src/stringConcept/findCase.java
@@ -1,2 +1,17 @@
-package stringConcept;public class findCase {
+package stringConcept;
+
+public class findCase {
+    public static void main(String[] args) throws Exception {
+        String str=null;
+        if(str == null || str.equals("")){
+           throw new Exception("String shd not be empty");
+        }
+        char c=str.charAt(0);
+        if(c>=97 && c<=122){
+            System.out.println(str.toLowerCase());
+
+        }
+        else
+            System.out.println(str.toUpperCase());
+    }
 }
