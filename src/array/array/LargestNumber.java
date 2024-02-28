85ab33f298e84fd570c3226f7340bb5366a09e60
Dhinesh Kumar Manivannan <93302687+Dhinesh045@users.noreply.github.com>
2024-02-28 21:34:24 +0530

    LargestNumber

---
+++ src/array/array/LargestNumber.java
@@ -0,0 +1,29 @@
+package array.array;
+
+import java.lang.reflect.Array;
+
+public class LargestNumber {
+        public static class SecondLargestInArrayExample{
+            public static int getSecondLargest(int[] a, int total){
+                int temp;
+                for (int i = 0; i < total; i++)
+                {
+                    for (int j = i + 1; j < total; j++)
+                    {
+                        if (a[i] > a[j])
+                        {
+                            temp = a[i];
+                            a[i] = a[j];
+                            a[j] = temp;
+                        }
+                    }
+                }
+                return a[total-1];
+            }
+            public static void main(String[] args){
+                int[] a ={1,2,5,6,37,2};
+                System.out.println("Second Largest: "+getSecondLargest(a,6));
+
+            }}
+    }
+
